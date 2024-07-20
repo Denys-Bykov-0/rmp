4022a5a76529b50a58b049411b4a3ecc31cce365
Volodymyr Fihurniak <67525028+VolodymyrFihurniak@users.noreply.github.com>
2024-07-20 12:34:58 +0300

    Update plugin interfaces (#166)

--- server/src/interfaces/iFilePlugin.ts
+++ server/src/interfaces/iFilePlugin.ts
@@ -2,11 +2,13 @@ import { FileDTO } from '@src/dtos/fileDTO';
 
 export abstract class iFilePlugin {
   pluginName!: string;
-  public abstract getSource: (url: string) => Promise<string>;
-  public abstract normalizeUrl: (url: string) => Promise<string>;
-  public abstract normalizeUrlPlaylist: (url: string) => string;
-  public abstract downloadFile: (
-    file: FileDTO,
-    source: string
-  ) => Promise<void>;
+  public abstract getSource: (url: string) => string;
+  public abstract normalizeUrl: (url: string) => string;
+  public abstract normalizePlaylistUrl: (url: string) => string;
+  public abstract requestFileProcessing: (
+    url: string,
+    userId: string,
+    playlistId: string
+  ) => void;
+  public abstract downloadFile: (file: FileDTO, routingKey: string) => void;
 }
--- services/file-coordinator/src/interfaces/filePlugin.ts
+++ services/file-coordinator/src/interfaces/filePlugin.ts
@@ -2,11 +2,13 @@ import { FileDTO } from '@dtos/fileDTO';
 
 export abstract class FilePlugin {
   pluginName!: string;
-  public abstract getSource: (url: string) => Promise<string>;
-  public abstract normalizeUrl: (url: string) => Promise<string>;
-  public abstract normalizeUrlPlaylist: (url: string) => string;
-  public abstract downloadFile: (
-    file: FileDTO,
-    source: string,
-  ) => Promise<void>;
+  public abstract getSource: (url: string) => string;
+  public abstract normalizeUrl: (url: string) => string;
+  public abstract normalizePlaylistUrl: (url: string) => string;
+  public abstract requestFileProcessing: (
+    url: string,
+    userId: string,
+    playlistId: string,
+  ) => void;
+  public abstract downloadFile: (file: FileDTO, routingKey: string) => void;
 }
